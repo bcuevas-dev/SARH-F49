3badccbe6e24cc859d5b0b9620778bf95cd263ed
Bienvenido Cuevas <bienvenido.cuevas@hotmail.com>
2017-09-13 22:08:05 -0400

    add webconfig and dbcontext
    Web config and dbcontext, enums folder, entity framework library

---
+++ SARH.Core/EF/SARHDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SARH.Core.EF
+{
+    public partial class SARHDbContext:DbContext
+    {
+        public SARHDbContext()
+            : base("name=SARHConnetions")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            throw new UnintentionalCodeFirstException();
+        }
+
+
+    }
+}
---
+++ SARH.Core/Enums/Enumerators.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SARH.Core.Enums
+{
+    public class Enumerators
+    {
+        public enum Status
+        {
+            Activ,
+            Inactive
+
+        }
+        public enum CivilStatus
+        {
+            free_Union,
+            married,
+            single,
+            divorced
+        }
+      
+    }
+}
